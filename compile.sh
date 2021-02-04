65c97f8d60fdc0fddba9c186b41c3420a9a662cc
_Szwendacz_ <maciejlebiest2@gmail.com>
2021-02-04 12:24:50 +0100

    CI/CD WiP 12

--- compile.sh
+++ compile.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 echo -n "Where I am :"
 pwd
-#echo "Checking for previous build and cleaning..."
-#build_directory="build"
-#if [ -d $build_directory ]
-#then
-#  echo "removing previous build directory..."
-#  rm -r build
-#fi
+echo "Checking for previous build and cleaning..."
+build_directory="build"
+if [ -d $build_directory ]
+then
+  echo "removing previous build directory..."
+  rm -r build
+fi
 
-#echo "Making dir "
-#mkdir $build_directory
+echo "Making dir "
+mkdir $build_directory
 echo "cmake configure..."
-#cmake . -B build
-cmake ..
+cmake . -B $build_directory
 echo "cmake compile..."
-cmake --build .
+cmake --build $build_directory
